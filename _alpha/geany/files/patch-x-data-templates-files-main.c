4e55f4fdbd44dee1442da5a496f38e5fcd7cc8d4
Oleg Borodin <onborodin@gmail.com>
2021-12-27 19:41:38 +0200

    added geany

---
+++ _alpha/geany/files/patch-x-data-templates-files-main.c
@@ -0,0 +1,12 @@
+--- ./data/templates/files/main.c.orig	2018-12-16 13:02:55.000000000 +0200
++++ ./data/templates/files/main.c	2019-03-26 14:48:33.380338000 +0200
+@@ -2,8 +2,7 @@
+ 
+ #include <stdio.h>
+ 
+-int main(int argc, char **argv)
+-{
++int main(int argc, char **argv) {
+ 	
+ 	return 0;
+ }
---
+++ _alpha/geany/files/patch-x-data-templates-files-main.cpp
@@ -0,0 +1,12 @@
+--- ./data/templates/files/main.cpp.orig	2019-03-26 14:48:25.307168000 +0200
++++ ./data/templates/files/main.cpp	2019-03-26 14:48:08.365742000 +0200
+@@ -0,0 +1,9 @@
++{fileheader}
++
++#include <iostream>
++
++int main(int argc, char **argv) {
++	
++	return 0;
++}
++
