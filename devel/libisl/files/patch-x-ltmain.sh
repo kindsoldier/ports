69f3ee1b4851e84ef05e8cc0f96e17994c33d7d5
Oleg Borodin <onborodin@gmail.com>
2022-11-20 13:26:48 +0200

    added libisl

---
+++ devel/libisl/files/patch-x-ltmain.sh
@@ -0,0 +1,36 @@
+--- ./ltmain.sh.orig	2020-12-31 16:34:27.000000000 +0200
++++ ./ltmain.sh	2022-11-20 13:23:47.353173000 +0200
+@@ -8906,13 +8906,13 @@
+ 	  #
+ 	  case $version_type in
+ 	  # correct linux to gnu/linux during the next big refactor
+-	  darwin|freebsd-elf|linux|osf|windows|none)
++	  darwin|linux|osf|windows|none)
+ 	    func_arith $number_major + $number_minor
+ 	    current=$func_arith_result
+ 	    age=$number_minor
+ 	    revision=$number_revision
+ 	    ;;
+-	  freebsd-aout|qnx|sunos)
++	  freebsd*|qnx|sunos)
+ 	    current=$number_major
+ 	    revision=$number_minor
+ 	    age=0
+@@ -8995,15 +8995,9 @@
+           esac
+ 	  ;;
+ 
+-	freebsd-aout)
++	freebsd*)
+ 	  major=.$current
+-	  versuffix=.$current.$revision
+-	  ;;
+-
+-	freebsd-elf)
+-	  func_arith $current - $age
+-	  major=.$func_arith_result
+-	  versuffix=$major.$age.$revision
++	  versuffix=.$current
+ 	  ;;
+ 
+ 	irix | nonstopux)
