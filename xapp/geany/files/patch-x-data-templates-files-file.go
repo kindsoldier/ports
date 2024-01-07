11db85913073ce8b1098fa024584d00b1a5fe3d1
Oleg Borodin <onborodin@gmail.com>
2024-01-07 17:35:48 +0200

    at work

--- xapp/geany/files/patch-x-data-templates-files-file.go
+++ xapp/geany/files/patch-x-data-templates-files-file.go
@@ -2,13 +2,21 @@
 +++ ./data/templates/files/file.go	2019-12-30 08:34:58.340297000 +0200
 @@ -0,0 +1,11 @@
 +{fileheader}
-+
 +package main
 +
 +import (
 +    "fmt"
 +)
 +
-+func main() {
++func run() error {
++    var err error
++    fmt.Printf("hello!\n")
++    return err
++}
 +
++func main() {
++    err := run()
++    if err != nil {
++        fmt.Printf("error: %v\n", err)
++    }
 +}
