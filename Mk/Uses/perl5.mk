d7324ff12e10cbefb97cdd734e7684ba98033d9d
Oleg Borodin <onborodin@gmail.com>
2025-08-28 08:07:35 +0200

    updated

--- Mk/Uses/perl5.mk
+++ Mk/Uses/perl5.mk
@@ -62,13 +62,11 @@ PERL_PORT=	perl530
 .    elif ${PERL5_DEFAULT} == 5.36
 #.include "${PORTSDIR}/lang/perl5.36/version.mk"
 PERL_PORT=	perl536
-PERL5_DEPEND=	perl>=5.30
-
+PERL5_DEPEND=	perl>=5.36
 .    elif ${PERL5_DEFAULT} == 5.40
 #.include "${PORTSDIR}/lang/perl5.40/version.mk"
 PERL_PORT=	perl540
 PERL5_DEPEND=	perl>=5.40
-
 .    elif ${PERL5_DEFAULT} == devel
 #.include "${PORTSDIR}/lang/perl5-devel/version.mk"
  Force PERL_PORT here in case two identical PERL_VERSION.
---
+++ perl5/corrpath.sed
@@ -0,0 +1,3 @@
+s,lib/perl5/site_perl/man/man3,%%PERL5_MAN3%%,
+s,lib/perl5/site_perl/man/man1,%%PERL5_MAN1%%,
+s,lib/perl5/site_perl,%%SITE_PERL%%,
